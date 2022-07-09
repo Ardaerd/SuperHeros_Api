ee1674d9f35d1a51d786b7c7fbb4f1950f651e76
Ardaerd <arderd@outlook.com>
2022-07-09 16:58:02 +0300

    AddHero method is added

---
+++ SuperHero_Api/Controllers/SuperHeroController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHero_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> heroes = new List<SuperHero>() {
+            new SuperHero
+            {
+                Name = "Spider-Man",
+                FirstName = "Peter",
+                LastName = "Parker",
+                Place = "New York City"
+            },
+            new SuperHero
+            {
+                Name = "Iron-Man",
+                FirstName = "Tony",
+                LastName = "Stark",
+                Place = "New York City"
+            }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(heroes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Get(int id)
+        {
+            var hero = heroes.Find(h => h.id == id);
+
+            if (hero == null)
+                return BadRequest("Hero not found.");
+
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heroes.Add(hero);
+            return Ok(heroes);
+        }
+
+
+
+      
+
+    }
+}
---
+++ SuperHero_Api/SuperHero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHero_Api
+{
+    public class SuperHero
+    {
+        public static int Id { get; set; }
+        public int id { get; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+        public SuperHero()
+        {
+            Id++;
+            id = Id;
+        }
+    }
+}
